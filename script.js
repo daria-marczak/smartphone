c03787964a0edb23d1f5bd35c927f1f4a7b2524a
daria marczak <marczak.ria@gmail.com>
2017-11-16 22:00:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function Phone(brand, price, color, display, identification) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.display = display;
+	this.identification = identification;
+}
+
+Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and its price is " + this.price + ". Its display is " + this.display +  " and its type of person identification is " + this.identification + ".")
+}
+
+var iPhoneX = new Phone("Apple", 2250, "space gray", "5.8''", "Face ID");
+var HuaweiP9Lite = new Phone("Huawei", 1940, "white", "4.8''", "finger ID");
+var SamsungGalaxyS6 = new Phone("Samsung", 1399, "black", "3.9''", "swipe");
+
+iPhoneX.printInfo();
+
+HuaweiP9Lite.printInfo();
+
+SamsungGalaxyS6.printInfo();
