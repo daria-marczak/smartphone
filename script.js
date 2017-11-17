1278ff7bd7edfdf203ebef1524b7e6a4092320b2
daria marczak <marczak.ria@gmail.com>
2017-11-17 18:48:54 +0100

    modified

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var priceAfterDiscount;
+var discount;
+
 function Phone(brand, price, color, display, identification) {
 	this.brand = brand;
 	this.price = price;
@@ -7,15 +10,23 @@ function Phone(brand, price, color, display, identification) {
 }
 
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and its price is " + this.price + ". Its display is " + this.display +  " and its type of person identification is " + this.identification + ".")
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and its price is " + this.price + ". Its display is " + this.display +  " and its type of person identification is " + this.identification + ".");
 }
 
-var iPhoneX = new Phone("Apple", 2250, "space gray", "5.8''", "Face ID");
-var HuaweiP9Lite = new Phone("Huawei", 1940, "white", "4.8''", "finger ID");
-var SamsungGalaxyS6 = new Phone("Samsung", 1399, "black", "3.9''", "swipe");
+Phone.prototype.applyDiscount = function getDiscount(discount) {
+	price = this.price - discount;
+	console.log(price)
+}
+
+
+var iPhoneX = new Phone("Apple", 2250, "space gray", "5.8in", "Face ID");
+var HuaweiP9Lite = new Phone("Huawei", 1940, "white", "4.8in", "finger ID");
+var SamsungGalaxyS6 = new Phone("Samsung", 1399, "black", "3.9in", "swipe");
 
 iPhoneX.printInfo();
 
 HuaweiP9Lite.printInfo();
 
 SamsungGalaxyS6.printInfo();
+
+iPhoneX.applyDiscount(200);
